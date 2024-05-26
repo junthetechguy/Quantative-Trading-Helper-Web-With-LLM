a2387dcd787826624a63bc65bd4a5f3ae8789319
junthetechguy <forestwoodleaves@gmail.com>
2024-05-26 23:00:56 +0900

    init: Initialize project(from Spring Initializer)

---
+++ src/test/java/com/capstone/quantativetradinghelperwebwithllm/QuantativetradinghelperwebwithllmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capstone.quantativetradinghelperwebwithllm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuantativetradinghelperwebwithllmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
