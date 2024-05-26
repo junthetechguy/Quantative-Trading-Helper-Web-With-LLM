c40e79f65eda547d82d35170b29df01d87103bbc
junthetechguy <forestwoodleaves@gmail.com>
2024-05-26 23:10:53 +0900

    chore: Change Java and Spring Boot version

--- src/main/java/com/capstone/quantativetradinghelperwebwithllm/QuantativetradinghelperwebwithllmApplication.java
+++ src/main/java/com/capstone/quantativetradinghelperwebwithllm/QuantativetradinghelperwebwithllmApplication.java
@@ -2,8 +2,9 @@ package com.capstone.quantativetradinghelperwebwithllm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class QuantativetradinghelperwebwithllmApplication {
 
 	public static void main(String[] args) {
