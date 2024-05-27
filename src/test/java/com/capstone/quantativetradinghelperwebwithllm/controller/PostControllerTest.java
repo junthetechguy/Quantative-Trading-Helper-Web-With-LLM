01776697e710588fd55f6f52413af86eb915fff6
juntheengineer <qusdudwns0162@gmail.com>
2024-05-27 21:45:16 +0900

    test: Add controller and service test

---
+++ src/test/java/com/capstone/quantativetradinghelperwebwithllm/controller/PostControllerTest.java
@@ -0,0 +1,17 @@
+package com.capstone.quantativetradinghelperwebwithllm.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class PostControllerTest {
+    @Test
+    public void 회원가입() throws Exception {
+        String userName = "userName";
+        String password = "password";
+
+        when(userService)
+    }
+}
